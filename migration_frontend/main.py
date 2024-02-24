04950a4403b235af206170471e52568da285f3cc
RealistikDash <realistikdash@gmail.com>
2024-02-24 19:54:20 +0000

    add: mysql transactions

--- migration_frontend/main.py
+++ migration_frontend/main.py
@@ -1,25 +1,75 @@
 from __future__ import annotations
 
+from databases import DatabaseURL
+from quart import g
 from quart import Quart
 from quart import render_template
+from quart import request
 
 from migration_frontend import config
+from migration_frontend.adapters.mysql import MySQLService
+from migration_frontend.repository import Repository
+
+
+async def render_page(
+    path: str,
+    title: str,
+    **kwargs,
+) -> str:
+    # This might be a bit unsafe for social engineering but oh wellll.
+    success = request.args.get("success")
+    error = request.args.get("error")
+    warning = request.args.get("warning")
+
+    return await render_template(
+        path,
+        title=title,
+        success=success,
+        error=error,
+        warning=warning,
+        **kwargs,
+    )
+
+
+def repo() -> Repository:
+    return Repository(g.sql)
 
 
 def configure_routes(app: Quart) -> None:
     @app.route("/")
     async def main():
-        return await render_template(
+        user_count = await repo().get_member_count()
+
+        return await render_page(
             "home.html",
             title="Home",
+            user_count=user_count,
         )
 
     @app.route("/password/migrate")
     async def migrate_password():
-        return await render_template(
+        return await render_page(
             "tools/password_migrate.html",
             title="Migrate Password",
         )
+
+
+def configure_mysql(app: Quart) -> None:
+    mysql = MySQLService(
+        DatabaseURL(
+            f"mysql://{config.MYSQL_USER}:{config.MYSQL_PASSWORD}@{config.MYSQL_HOST}:{config.MYSQL_PORT}/{config.MYSQL_DATABASE}",
+        ),
+    )
+
+    @app.before_request
+    async def transaction_start():
+        g.sql = await mysql.transaction().__aenter__()
+
+    @app.after_request
+    async def transaction_end(r):
+        await g.sql.__aexit__()
+
+        return r
 
 
 def init_app() -> Quart:
